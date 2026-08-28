8c27d9fd870c5510158ec51d2624143ad2892ff7
Juan Pelaez <jap447@cornell.edu>
2026-08-27 22:42:02 -0400

    Error measurements

--- assignment_0.py
+++ assignment_0.py
@@ -16,34 +16,54 @@ params = {
 # some set-up
 initial_state = np.array([np.pi / 4, 0.0])
 
-timestep = 1e-5
+timesteps = np.logspace(-5, -2, 20)
 sim_time = 5.0
 
-n_timesteps = int(sim_time / timestep) + 1
-time_traj = np.arange(n_timesteps) * timestep
-state_traj = np.zeros((2, n_timesteps))
-state_traj[:, 0] = initial_state
+energy_errors = []
 
-# simulation loop
-for step, t in enumerate(time_traj[:-1]):
-    state_traj[:, step + 1] = state_traj[:, step] + timestep * model.dynamics(
-        t, state_traj[:, step], params
-    )
+for timestep in timesteps:
 
-# sanity check the energies: since there is no actuation, and no damping, total energy should stay
-# constant. If we turn on the damping coefficient, it should slowly bleed out energy until it comes to
-# a stand-still.
+    n_timesteps = int(sim_time / timestep) + 1
+    time_traj = np.arange(n_timesteps) * timestep
+    state_traj = np.zeros((2, n_timesteps))
+    state_traj[:, 0] = initial_state
 
-potential_energy, kinetic_energy = model.calculate_energy(state_traj, params)
+    # simulation loop
+    for step, t in enumerate(time_traj[:-1]):
+        state_traj[:, step + 1] = state_traj[:, step] + timestep * model.dynamics(
+            t, state_traj[:, step], params
+        )
+
+    # sanity check the energies: since there is no actuation, and no damping, total energy should stay
+    # constant. If we turn on the damping coefficient, it should slowly bleed out energy until it comes to
+    # a stand-still.
+
+    kinetic_energy, potential_energy = model.calculate_energy(state_traj, params)
+    total_energy = kinetic_energy + potential_energy
+
+    energy_error = np.max(np.abs(total_energy - total_energy[0]))
+
+    energy_errors.append(energy_error)
+
+    print(f"dt = {timestep:.6f} s, max energy error = {energy_error:.6e} J")
+
+#    plt.figure()
+#    plt.plot(time_traj, potential_energy, label="Potential energy")
+#    plt.plot(time_traj, kinetic_energy, label="Kinetic energy")
+#    plt.plot(time_traj, potential_energy + kinetic_energy, label="Total energy")
+#    plt.xlabel("Time (s)")
+#    plt.ylabel("Energy (J)")
+#    plt.title("Pendulum energy")
+#    plt.legend()
+#    plt.tight_layout()
+#    plt.show()
 
 plt.figure()
-plt.plot(time_traj, potential_energy, label="Potential energy")
-plt.plot(time_traj, kinetic_energy, label="Kinetic energy")
-plt.plot(time_traj, potential_energy + kinetic_energy, label="Total energy")
-plt.xlabel("Time (s)")
-plt.ylabel("Energy (J)")
-plt.title("Pendulum energy")
-plt.legend()
+plt.plot(timesteps, energy_errors, "o-")
+plt.xlabel("Timestep (s)")
+plt.ylabel("Maximum energy error (J)")
+plt.title("Energy error vs. timestep")
+plt.grid(True)
 plt.tight_layout()
 plt.show()
 
