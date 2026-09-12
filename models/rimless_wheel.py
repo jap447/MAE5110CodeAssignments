49b373f840d3ae60c3c70fae1762ffbfdfed4121
Juan Pelaez <jap447@cornell.edu>
2026-09-12 14:30:46 -0400

    Poincare map fixed and code cleaned

--- models/rimless_wheel.py
+++ models/rimless_wheel.py
@@ -40,12 +40,13 @@ def calculate_energy(state, height_traj, params):
     length = params["length"]
     mass = params["mass"]
 
-    angle = state[0]  # indexes entire row "vectorized" if state is (2, N)
+    angle = state[0]
     angular_velocity = state[1]
-    
+
     kinetic_energy = 0.5 * mass * (length * angular_velocity) ** 2
     potential_energy = mass * gravity * (length * np.cos(angle) + height_traj)
     return kinetic_energy, potential_energy
+
 
 def walking_converged(impact_velocities, window=5, tol=1e-3):
     if len(impact_velocities) < window:
@@ -54,15 +55,14 @@ def walking_converged(impact_velocities, window=5, tol=1e-3):
     recent = np.asarray(impact_velocities[-window:])
     return np.max(np.abs(np.diff(recent))) < tol
 
-def refine_impact(previous_state, t, coarse_dt, fine_dt, params, integrator):
 
+def refine_impact(previous_state, t, coarse_dt, fine_dt, params, integrator):
     state = np.array(previous_state, copy=True)
     local_t = t
 
     n_fine_steps = int(np.ceil(coarse_dt / fine_dt))
 
     for _ in range(n_fine_steps):
-
         next_state = integrator(pendulum_dynamics, local_t, state, fine_dt, params)
 
         if detect_event(state, next_state, params):
@@ -72,3 +72,37 @@ def refine_impact(previous_state, t, coarse_dt, fine_dt, params, integrator):
         local_t += fine_dt
 
     return state
+
+
+def next_impact_velocity(
+    current_velocity,
+    params,
+    timestep,
+    impact_timestep,
+    integrator,
+    post_impact_angle,
+    pendulum_dynamics_func,
+    detect_event_func,
+    refine_impact_func,
+    reset_impact_func,
+):
+    state = np.array([-(post_impact_angle), current_velocity], dtype=float)
+    t = 0.0
+
+    while True:
+        next_state = integrator(pendulum_dynamics_func, t, state, timestep, params)
+
+        if detect_event_func(state, next_state, params):
+            refined_state = refine_impact_func(
+                state,
+                t,
+                timestep,
+                impact_timestep,
+                params,
+                integrator,
+            )
+            reset_state = reset_impact_func(refined_state, params)
+            return float(reset_state[1])
+
+        state = next_state
+        t += timestep
